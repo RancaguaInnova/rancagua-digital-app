8e0b0fabdc110ee7d894ef4085340a67a185f5a5
Jaime Perez <jaime.e.perez.r@hotmail.com>
2021-09-08 10:21:11 -0300

    update events

--- src/providers/redux/actions/event.ts
+++ src/providers/redux/actions/event.ts
@@ -1,34 +1,34 @@
-import { Dispatch } from 'redux'
-import { Event } from 'core/interfaces/event'
-import types from 'providers/redux/types'
-import axios from 'axios'
-import { url } from 'providers/urlprovider'
+import { Dispatch } from "redux";
+import { Event } from "core/interfaces/event";
+import types from "providers/redux/types";
+import axios from "axios";
+import { url } from "providers/urlprovider";
 
-axios.defaults.baseURL = url
+axios.defaults.baseURL = url;
 
-export const GetListEvents = () => {
-  console.log('test')
+export const GetListEvents = (date:string) => {
+  console.log("test");
   return async (dispach: Dispatch) => {
     try {
-      let events: Event[] = []
-      const { data, status } = await axios.get(`/city/events`)
+      let events: Event[] = [];
+      const { data, status } = await axios.get(`/city/events/app/list?date=${date}&limit=10`);
       if (status === 200) {
-        events = data.data
-        dispach(eventList(events))
+        events = data;
+        dispach(eventList(events));
       } else {
-        console.log(status)
+        console.log(status);
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-}
+  };
+};
 
 export const eventList = (events: Event[]) => {
   return {
     type: types.GET_LIST_EVENTS,
     payload: {
-      eventList: events
-    }
-  }
-}
+      eventList: events,
+    },
+  };
+};
